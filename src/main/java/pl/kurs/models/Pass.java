ebf8816d4f4552718ced038608675c340586d984
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-04-27 12:05:57 +0200

    Pass cooked

--- src/main/java/pl/kurs/models/Pass.java
+++ src/main/java/pl/kurs/models/Pass.java
@@ -1,4 +1,30 @@
 package pl.kurs.models;
 
-public class Pass {
+public enum Pass {
+    BASIC("podstawowy", 70, 9, 1),
+    STANDARD("standardowy", 105, 14, 1),
+    EXTENDED("rozszerzony",140, 20, 2),
+    PREMIUM("premium", 180, 1000, 1000);
+
+    private String name;
+    private double price;
+    private int entersNumber;
+    private int enterPerDay;
+
+    Pass(String name, double price, int entersNumber, int enterPerDay) {
+        this.name = name;
+        this.price = price;
+        this.entersNumber = entersNumber;
+        this.enterPerDay = enterPerDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Pass{" +
+                "name=" + name +
+                ", price=" + price +
+                ", entersNumber=" + entersNumber +
+                ", enterPerDay=" + enterPerDay +
+                '}';
+    }
 }
