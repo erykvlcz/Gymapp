e1d07accaa6e55981b9f0791e5b29e65770d3a6f
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-04-27 12:23:34 +0200

    Member edited

--- src/main/java/pl/kurs/app/Runner.java
+++ src/main/java/pl/kurs/app/Runner.java
@@ -1,12 +1,13 @@
 package pl.kurs.app;
 
+import pl.kurs.models.Member;
 import pl.kurs.models.Pass;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println(Pass.BASIC);
-        System.out.println(Pass.STANDARD);
-        System.out.println(Pass.EXTENDED);
-        System.out.println(Pass.PREMIUM);
+        Member franek = Member.create("Franek", Pass.EXTENDED);
+        System.out.println(franek);
+
+
     }
 }
